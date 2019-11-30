a09c9dbb2ae0e41bfad21f0431e01888c4f265ce
Luis Ascencio <luismiguelascencio@gmail.com>
2019-11-30 11:15:00 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import tone from "tone";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          MusicFlow
-        </a>
-      </header>
-    </div>
-  );
+export class Button extends Component {
+  render() {
+    return (
+      <div>
+        <button />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Button;
