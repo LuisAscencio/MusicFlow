0eb4157d5f280cd555bb36971bdeaa9d6d5a4626
Luis Ascencio <luismiguelascencio@gmail.com>
2019-12-03 15:04:42 -0500

    main component for magenta model

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import tone from "tone";
+import magenta from "@magenta/music/node/core";
 
 export class Button extends Component {
   render() {
     return (
-      <div>
-        <button />
+      <div class="jumbotron">
+        <h1 class="card-title">MusicFlow</h1>
       </div>
     );
   }
